9d67b8802536b1c596ef582f291e6c2ca3b6fd4f
lijf <bujue110@163.com>
2017-04-21 17:34:59 +0800

    l

--- src/main/java/com/nepu/controller/TeacherController.java
+++ src/main/java/com/nepu/controller/TeacherController.java
@@ -11,6 +11,7 @@ import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.*;
 
+import javax.persistence.criteria.CriteriaBuilder;
 import javax.servlet.http.HttpServletRequest;
 import java.util.ArrayList;
 import java.util.HashMap;
@@ -31,19 +32,31 @@ public class TeacherController {
 
     //添加试题
     @PostMapping(value = "/addSubject")
-    public @ResponseBody Map<String, Object> addSubject(HttpServletRequest request){
-        String content = request.getParameter("content");
-        String aItem = request.getParameter("aItem");
-        String bItem = request.getParameter("bItem");
-        String cItem = request.getParameter("cItem");
-        String dItem = request.getParameter("dItem");
-        String answer = request.getParameter("answer");
-        String analysis = request.getParameter("analysis");
-
-
-
+    public @ResponseBody Map<String, Object> addSubject(HttpServletRequest request) throws Exception{
         Map<String, Object> resultMap = new HashMap<>();
-        resultMap.put("resultString","添加成功！");
+        try {
+            String content = request.getParameter("content");
+            String aItem = request.getParameter("aItem");
+            String bItem = request.getParameter("bItem");
+            String cItem = request.getParameter("cItem");
+            String dItem = request.getParameter("dItem");
+            String answer = request.getParameter("answer");
+            String analysis = request.getParameter("analysis");
+            Integer subjectType = Integer.parseInt(request.getParameter("subjectType"));
+            Subject subject = new Subject();
+            subject.setContent(content);
+            subject.setaItem(aItem);
+            subject.setbItem(bItem);
+            subject.setcItem(cItem);
+            subject.setdItem(dItem);
+            subject.setAnswer(answer);
+            subject.setAnalysis(analysis);
+            subject.setSubjectId(subjectType);
+            subjectDao.save(subject);
+            resultMap.put("resultString","添加成功！");
+        }catch (Exception e){
+            resultMap.put("resultString","添加失败！");
+        }
         return resultMap;
     }
 
@@ -77,5 +90,40 @@ public class TeacherController {
     //构建PageRequest
     private PageRequest buildPageRequest(int pageNumber, int pageSize) {
         return new PageRequest(pageNumber - 1, pageSize, null);
+    }
+
+    //添加试题到试题篮
+    @PostMapping(value = "/addSubjectToPaper")
+    public @ResponseBody Map<String, Object> addSubjectToPaper(HttpServletRequest request){
+        Map<String, Object> resultMap = new HashMap<>();
+        Integer subjectId = Integer.parseInt(request.getParameter("subjectId"));
+        //List<String> subjectIds = new ArrayList<>();
+        ArrayList<Integer> subjects =  (ArrayList<Integer>) request.getSession().getAttribute("subjects");
+        if (subjects == null){
+            subjects = new ArrayList<>();
+            subjects.add(subjectId);
+            resultMap.put("resultString","添加成功了！");
+        }else {
+            if (subjects.contains(subjectId)){
+                resultMap.put("resultString","已经有这道题了！");
+            }else {
+                subjects.add(subjectId);
+                resultMap.put("resultString","添加成功了！");
+            }
+        }
+        request.getSession().setAttribute("subjects",subjects);
+        //向页面输出两个超链接 一个是继续购物 ，一个是结算
+        return resultMap;
+    }
+
+    @GetMapping(value = "/queryCart")
+    public String queryCart(HttpServletRequest request){
+        ArrayList<Integer> subjects =  (ArrayList<Integer>) request.getSession().getAttribute("subjects");
+        if (subjects == null){
+            //试题篮为空
+        }else {
+            for (int i)
+        }
+        return "/teacher/cart";
     }
 }
